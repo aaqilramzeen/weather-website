5402398901bf685702fdec1df177b08100b956b2
aaqilramzeen <64853911+aaqilramzeen@users.noreply.github.com>
2020-07-30 13:00:24 +0530

    added new data for the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location!', undefined);
         } else {
             callback(undefined, 
-                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees.'    
+                body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees with a humidity of ' + body.current.humidity + '%.'    
             );
         }
     });
